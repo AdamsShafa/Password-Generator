462de2a597ed31d3a120d158f3903576ff16ce50
AdamsShafa <adamzshafa@gmail.com>
2023-01-10 21:05:16 +0000

    Arranged

--- script.js
+++ script.js
@@ -196,7 +196,7 @@ function generatePassword() {
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Input password to the 'password input'
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
